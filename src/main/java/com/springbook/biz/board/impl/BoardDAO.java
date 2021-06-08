6ab9464c73202fcc8330076d9d3382b5cb0241d9
DESKTOP-19KJDK8\chban <yebitu@gmail.com>
2021-06-08 17:04:41 +0900

    p394 @SessionAttributes

--- src/main/java/com/springbook/biz/board/impl/BoardDAO.java
+++ src/main/java/com/springbook/biz/board/impl/BoardDAO.java
@@ -21,7 +21,7 @@ public class BoardDAO {
 	
 	//SQL 명령어들
 	private final String BOARD_INSERT = "insert into board(seq, title, writer, content) values((select nvl(max(seq), 0)+1 from board), ?,?,?)";
-	private final String BOARD_UPDATE = "update board set title=?, content=? where seq=?";
+	private final String BOARD_UPDATE = "update board set title=?, content=?, writer=? where seq=?";
 	private final String BOARD_DELETE = "delete board where seq=?";
 	private final String BOARD_GET= "select * from board where seq=?";
 	private final String BOARD_LIST = " select * from board order by seq desc";
@@ -52,7 +52,8 @@ public class BoardDAO {
 			stmt = conn.prepareStatement(BOARD_UPDATE);
 			stmt.setString(1, vo.getTitle());
 			stmt.setString(2, vo.getContent());
-			stmt.setInt(3, vo.getSeq());
+			stmt.setString(3, vo.getWriter());
+			stmt.setInt(4, vo.getSeq());
 			stmt.executeUpdate();
 		} catch (Exception e) {
 			e.printStackTrace();
--- src/main/java/com/springbook/view/board/BoardController.java
+++ src/main/java/com/springbook/view/board/BoardController.java
@@ -8,11 +8,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
 
 @Controller
+@SessionAttributes("board")
 public class BoardController {
 	
 	// 검색 조건 목록 설정
@@ -33,7 +35,13 @@ public class BoardController {
 	
 	// 글 수정
 	@RequestMapping("/updateBoard.do")
-	public String updateBoard(BoardVO vo, BoardDAO boardDAO) {	
+	public String updateBoard(@ModelAttribute("board") BoardVO vo, BoardDAO boardDAO) {	
+		System.out.println("번호: "+ vo.getSeq());
+		System.out.println("제목: "+ vo.getTitle());
+		System.out.println("작성자: "+ vo.getWriter());
+		System.out.println("내용: "+ vo.getContent());
+		System.out.println("등록일: "+ vo.getRegDate());
+		System.out.println("조회수: "+ vo.getCnt());
 		boardDAO.updateBoard(vo);
 		return "getBoardList.do";
 	}
